569b320e37693e01fd7c7e8e800a3da638bacfb0
Sheldon Hearn <sheldonh@starjuice.net>
2016-02-11 15:32:12 +0200

    Try fix jruby

--- spec/go_spec.rb
+++ spec/go_spec.rb
@@ -8,8 +8,7 @@ describe "TLSv1.2 HIGH cipher support" do
 
   it "is supported" do
     expect {
-      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_NO_COMPRESSION
-      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ciphers] = "TLSv1.2:!aNULL:!eNULL"
+      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ciphers] = "TLSv1.2+HIGH:!aNULL:!eNULL"
       OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = "TLSv1_2"
       @json = URI.parse("https://www.howsmyssl.com/a/check").read
     }.to_not raise_error
