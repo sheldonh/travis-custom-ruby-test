ef0aed555083692f0cbc06b692d9bb1d3308c74c
Sheldon Hearn <sheldonh@starjuice.net>
2016-02-11 13:55:47 +0200

    Iterate

--- spec/go_spec.rb
+++ spec/go_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
-describe "TLSv1.2" do
+require "json"
+require "open-uri"
+require "openssl"
+
+describe "TLSv1.2 HIGH cipher support" do
 
   it "is supported" do
     expect {
-      require "openssl"
-      OpenSSL::SSL::SSLContext.new.ssl_version = :TLSv1_2
+      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:options] |= OpenSSL::SSL::OP_NO_COMPRESSION
+      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ciphers] = "TLSv1.2:!aNULL:!eNULL"
+      OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = "TLSv1_2"
+      @json = URI.parse("https://www.howsmyssl.com/a/check").read
     }.to_not raise_error
+    rating = JSON.parse(@json)["rating"]
+    expect(rating).to eql "Probably Okay"
   end
 
 end
